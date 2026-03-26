c661bbcda5408abde7ecc7d736c1a8412c9c694d
aryan-Patel-web <aryanpatel77462@gmail.com>
2026-03-26 14:53:41 +0530

    using cookiecutter  i created folder template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='production-sentiment-analysis-end-to-end-mlops  using tools like dvc , mlflow , dagshub , aws s3 ec2 ecr eks , github actions promthues , grafana for monitoring and visualixation',
+    author='Aryan patel',
+    license='MIT',
+)
