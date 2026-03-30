816a3e077fc2898ae1513b8ac1743ff1cad9ba8a
aryan-Patel-web <aryanpatel77462@gmail.com>
2026-03-31 01:59:31 +0530

    dvc.yaml file updated

--- src/model/model_evaluation.py
+++ src/model/model_evaluation.py
@@ -133,8 +133,11 @@ def main():
                     mlflow.log_param(param_name, param_value)
             
             # Log model to MLflow
-            # mlflow.sklearn.log_model(clf, "model")
-            mlflow.sklearn.log_model(sk_model=clf,artifact_path="model")
+            # mlflow.sklearn.log_model(sk_model=clf, artifact_path="model")
+            mlflow.log_artifact("./models/model.pkl", artifact_path="model")
+            
+            # mlflow.sklearn.log_model(sk_model=clf,artifact_path="model")
+            print("MODEL LOGGED SUCCESSFULLY")
             
             # Save model info
             save_model_info(run.info.run_id, "model", 'reports/experiment_info.json')
